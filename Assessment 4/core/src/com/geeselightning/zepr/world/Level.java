bb9f74ef336b206f69dae4caa51b2371ebd468ee
Thomas Turner <tt792@york.ac.uk>
2019-04-28 16:59:09 +0100

    Fixed them damn maps

--- Assessment 4/core/src/com/geeselightning/zepr/world/Level.java
+++ Assessment 4/core/src/com/geeselightning/zepr/world/Level.java
@@ -37,7 +37,7 @@ public class Level {
 		RONCOOKE("Ron Cooke Hub", "roncooke", 5),
 		
 		//Assessment 4: Zombie levels
-		ZOMBIE1("Level 1", "chemisty", 10),
+		ZOMBIE1("Level 1", "chemistry", 10),
 		ZOMBIE2("Level 2", "zombiefight", 11);
 		
 		// Human-readable level name
